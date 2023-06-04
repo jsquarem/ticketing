71f582d4c39a3958cca9b085c29a2433208c34d0
jmartymar <jeffjmart@gmail.com>
2023-06-04 12:01:57 -0500

    routes and middlewares implemented

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -11,7 +12,14 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+	cookieSession({
+		signed: false,
+		secure: true
+	})
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -19,22 +27,26 @@ app.use(signoutRouter);
 app.use(signupRouter);
 
 app.all('*', async (req, res) => {
-  throw new NotFoundError();
+	throw new NotFoundError();
 });
 
 app.use(errorHandler);
 
 const start = async () => {
-  try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-    console.log('MongoDB connected on port 27017');
-  } catch (err) {
-    console.log(err);
-  }
+	if (!process.env.JWT_KEY) {
+		throw new Error('JWT_KEY must be defined');
+	}
 
-  app.listen(3000, () => {
-    console.log('Listening on port 3000');
-  });
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+		console.log('MongoDB connected on port 27017');
+	} catch (err) {
+		console.log(err);
+	}
+
+	app.listen(3000, () => {
+		console.log('Listening on port 3000!');
+	});
 };
 
 start();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
@@ -13,14 +15,10 @@ router.post(
     body('password')
       .trim()
       .isLength({ min: 6, max: 20 })
-      .withMessage('Password must be between 6 and 20 characters.')
+      .withMessage('Password must be between 6 and 20 characters.'),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
@@ -30,8 +28,22 @@ router.post(
     const user = User.build({ email, password });
     await user.save();
 
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!,
+    );
+
+    // Store JWT in session
+    req.session = {
+      jwt: userJwt,
+    };
+
     res.status(201).send(user);
-  }
+  },
 );
 
 export { router as signupRouter };
